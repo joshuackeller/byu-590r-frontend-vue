959bf9aba243b56791915f80c92a222465e205a9
joshuackeller <joshuackeller@gmail.com>
2023-04-26 08:56:14 -0600

    change api url

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,5 +1,5 @@
-//export const BASE_URL = "https://byu-backend-php.fly.dev/";
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "https://byu-backend-php.fly.dev/";
+//export const BASE_URL = "http://localhost:8000/";
 
 const API_URL = BASE_URL + "api/";
 export default API_URL;
